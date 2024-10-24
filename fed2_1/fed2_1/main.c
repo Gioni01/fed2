ab1b6cbe0bb2461b201eca01d22e5cc8f15827dc
Gioni01 <147532598+Gioni01@users.noreply.github.com>
2024-10-24 08:43:53 +0200

    Add files via upload

---
+++ fed2_1/fed2_1/main.c
@@ -0,0 +1,18 @@
+/*
+ * fed2_1.c
+ *
+ * Created: 2024. 10. 24. 8:36:20
+ * Author : Acer
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
